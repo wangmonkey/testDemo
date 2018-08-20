bb4a15b59681bce03e850a497cadfcedd9b30883
wxueyanghj@163.com <wxueyanghj@163.com>
2018-08-20 10:15:18 +0800

    add

--- src/test/java/com/wang/testproblems/Solution.java
+++ src/test/java/com/wang/testproblems/Solution.java
@@ -2,6 +2,7 @@ package com.wang.testproblems;
 
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        System.out.println("hello,world !");
         return null;
     }
 }
