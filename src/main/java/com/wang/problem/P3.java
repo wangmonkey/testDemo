30df0b34383093c8e0bafb47b66aa62350e5b827
wangxueyang <wxueyanghj@163.com>
2018-08-20 16:27:26 +0800

    最长非重复字符串

--- src/main/java/com/wang/problem/P3.java
+++ src/main/java/com/wang/problem/P3.java
@@ -1,5 +1,6 @@
 package com.wang.problem;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -27,17 +28,18 @@ public class P3 {
             if (map.containsValue(chars[i])){
                 map.clear();
                 map.put(count++ , chars[i]);
-                i = 0;
-                s = s.substring(s.indexOf(chars[i]) + 1);
-
+                s = s.substring(s.indexOf(chars[i])+1);
+                i = -1;
+                System.out.println(s + "  :  " + maxLength);
                 continue;
             } else {
                 map.put(++count, chars[i] );
+                System.out.println(s + "  :  " + maxLength);
             }
             if (map.size() > maxLength){
                 maxLength = map.size();
             }
-            System.out.println(s + "  :  " + maxLength);
+
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
@@ -47,7 +49,20 @@ public class P3 {
         return maxLength;
     }
 
+    public static int lengthOfLongestSubstring2(String s) {
+        int n = s.length(), count = 0;
+        Map<Character, Integer> map = new HashMap<>();
+            for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            count = Math.max(count, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring2("abcabcbb"));
     }
 }
